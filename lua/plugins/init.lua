19dd2a22232383a9e7cf554c8129e826c0b4c12a
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-12-30 22:19:06 +0600

    feat(plugins): configure Treesitter and additional plugins
    - Enable Treesitter for all languages (JS/TS/React/Rust/C++/Python/Nix/Markdown/LaTeX)
    - Add nvim-ts-autotag for auto-closing HTML/JSX tags
    - Add rust.vim for Rust-specific tooling
    - Add Trouble for better diagnostics viewer
    - Remove LSPSaga due to deprecated API warnings in Neovim 0.11+
    - Enable incremental selection and text objects
    - Configure syntax highlighting and indentation
    
    Treesitter parsers installed:
    - Web: html, css, scss, javascript, typescript, tsx, jsx, json, jsonc, graphql
    - Systems: rust, toml, c, cpp, cmake
    - Python: python
    - Config: nix, yaml, bash
    - Docs: markdown, markdown_inline, latex, bibtex
    - Utils: regex, gitignore, gitcommit, diff

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,28 +1,132 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre", -- Enable format on save
+        opts = require "configs.conform",
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end,
+    },
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                -- Core
+                "vim",
+                "lua",
+                "vimdoc",
+                "luadoc",
+                "printf",
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+                -- Web Development (JS/TS/React/Next/Vite/Tailwind)
+                "html",
+                "css",
+                "scss",
+                "javascript",
+                "typescript",
+                "tsx",
+                "jsx",
+                "json",
+                "jsonc",
+                "graphql",
+
+                -- Systems Programming
+                "rust",
+                "toml", -- Rust Cargo.toml
+                "c",
+                "cpp",
+                "cmake",
+
+                -- Python
+                "python",
+
+                -- Nix
+                "nix",
+
+                -- Documentation
+                "markdown",
+                "markdown_inline",
+                "latex",
+                "bibtex",
+
+                -- Other useful
+                "bash",
+                "yaml",
+                "regex",
+                "gitignore",
+                "gitcommit",
+                "diff",
+            },
+
+            highlight = {
+                enable = true,
+                use_languagetree = true,
+            },
+
+            indent = {
+                enable = true,
+            },
+
+            -- Enable incremental selection
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+
+            -- Enable text objects
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                },
+            },
+        },
+    },
+
+    -- Additional useful plugins for development
+    {
+        "windwp/nvim-ts-autotag",
+        ft = {
+            "html",
+            "javascript",
+            "typescript",
+            "javascriptreact",
+            "typescriptreact",
+            "xml",
+        },
+        config = function()
+            require("nvim-ts-autotag").setup()
+        end,
+    },
+
+    -- LSPSaga removed due to deprecated API warnings in Neovim 0.11+
+    -- Built-in LSP UI is excellent, no need for additional plugin
+
+    -- Rust-specific tools
+    {
+        "rust-lang/rust.vim",
+        ft = "rust",
+        init = function()
+            vim.g.rustfmt_autosave = 1
+            vim.g.rustfmt_emit_files = 1
+            vim.g.rustfmt_fail_silently = 0
+        end,
+    },
+
+    -- Better diagnostics
+    {
+        "folke/trouble.nvim",
+        cmd = "Trouble",
+        opts = {},
+    },
 }
