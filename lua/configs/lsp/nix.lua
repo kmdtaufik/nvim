b3f53f88ff29f1f03f058a93bdba5abf163085d0
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-12-31 20:48:53 +0600

    chore: fix nixd completions

--- lua/configs/lsp/nix.lua
+++ lua/configs/lsp/nix.lua
@@ -2,54 +2,54 @@
 -- Using nixd for Nix language support
 
 vim.lsp.config.nixd = {
-    cmd = { "nixd" },
-    filetypes = { "nix" },
-    root_markers = {
-        "flake.nix",
-        "default.nix",
-        "shell.nix",
-        ".git"
-    },
-    settings = {
-        nixd = {
-            -- Formatting with alejandra
-            formatting = {
-                command = { "alejandra" },
-            },
+  cmd = { "nixd" },
+  filetypes = { "nix" },
+  root_markers = {
+    "flake.nix",
+    "shell.nix",
+    ".git"
+  },
+  settings = {
+    nixd = {
+      -- Formatting with alejandra
+      formatting = {
+        command = { "alejandra" },
+      },
 
-            -- Diagnostics
-            diagnostic = {
-                suppress = {},
-            },
+      -- Diagnostics
+      diagnostic = {
+        suppress = {},
+      },
 
-            -- Nixpkgs options
-            nixpkgs = {
-                -- Expression to get nixpkgs
-                expr = "import <nixpkgs> { }",
-            },
+      -- Nixpkgs options
+      nixpkgs = {
+        -- Expression to get nixpkgs
+        expr = "import <nixpkgs> { }",
+      },
 
-            -- Options for NixOS, home-manager, etc.
-            options = {
-                -- Enable options completion
-                enable = true,
+      -- Options for NixOS, home-manager, etc.
+      options = {
+        -- Enable options completion
+        -- enable = true,
 
-                -- NixOS options
-                nixos = {
-                    expr = '(builtins.getFlake "/etc/nixos").nixosConfigurations.<hostname>.options',
-                },
-
-                -- Home Manager options
-                ["home-manager"] = {
-                    expr = '(builtins.getFlake "/etc/nixos").homeConfigurations.<username>.options',
-                },
-            },
+        -- NixOS options
+        nixos = {
+          expr = '(builtins.getFlake ("git+file://" + toString ~/abyss)).nixosConfigurations.nixos.options',
         },
+
+        -- -- Home Manager options
+        home_manager = {
+          expr =
+          '(builtins.getFlake ("git+file://" + toString ~/abyss)).nixosConfigurations.nixos.options.home-manager.users.type.getSubOptions []',
+        },
+      },
     },
+  },
 }
 
 -- Enable nixd
 vim.lsp.enable("nixd")
 
 return {
-    servers = { "nixd" },
+  servers = { "nixd" },
 }
