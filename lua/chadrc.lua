73802cf1807469e6bb658f533090f036732f6a73
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-12-31 20:49:14 +0600

    chore: add dashboard on startup

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -8,13 +8,13 @@ local M = {}
 M.base46 = {
   theme = "onedark",
   transparency = true,
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
 }
 
--- M.nvdash = { load_on_startup = true }
+M.nvdash = { load_on_startup = true }
 -- M.ui = {
 --       tabufline = {
 --          lazyload = false
