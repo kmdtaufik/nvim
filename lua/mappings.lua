887b9d8d9818dd6cf9db20d43db039060989da43
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2026-01-01 00:25:11 +0600

    chore: some changes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,7 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 -- LSP Keymaps
 map("n", "gD", vim.lsp.buf.declaration, { desc = "LSP Go to declaration" })
