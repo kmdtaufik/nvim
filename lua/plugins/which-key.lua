ae6684c9eeecfe59dc1814ff2f3247c8bc8c892e
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2026-02-06 00:37:54 +0600

    chore: change which-key preset

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,9 @@
+-- ~/.config/nvim/lua/plugins/which-key.lua
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      preset = "modern", -- Options: "classic", "modern", "helix"
+    },
+  },
+}
